4ddb18a7b091be54893705623ef2e5695d509544
Philip Karns <pkarns@deloitte.com>
2018-11-11 13:25:59 -0500

    Added simple pagination to controller

--- SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -12,15 +12,25 @@ namespace SportsStore.WebUI.Controllers
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        
+        // 4 products per page
+        public int PageSize = 4;
 
         public ProductController(IProductRepository productRepository)
         {
             this.repository = productRepository;
         }
 
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Products);
+            //get the Products from the DB
+            return View(repository.Products
+                //order by primary key
+                .OrderBy(p => p.ProductID)
+                //skip over the products that occur before the start of the current page
+                .Skip((page - 1) * PageSize)
+                //take(show) the number of products specified by the PageSize field
+                .Take(PageSize));
         }
     }
 }
