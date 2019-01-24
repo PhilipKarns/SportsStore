15f2884577a7f8d3ac6e36a8de14ab3b2240af83
Philip Karns <pkarns@deloitte.com>
2019-01-24 15:02:04 -0500

    created and implemented a checkout process for items in the cart

--- SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -44,6 +45,13 @@ namespace SportsStore.WebUI.Infrastructure
 
             //tells Ninject to create instances of the EFProductRepository class to service requests for the IProductRepository interface
             kernel.Bind<IProductRepository>().To<EFProductRepository>();
+
+            EmailSettings emailSettings = new EmailSettings
+            {
+                WriteAsFile = bool.Parse(ConfigurationManager.AppSettings["Email.WriteAsFile"] ?? "false")
+            };
+
+            kernel.Bind<IOrderProcessor>().To<EmailOrderProcessor>().WithConstructorArgument("settings", emailSettings);
         }
     }
 }
